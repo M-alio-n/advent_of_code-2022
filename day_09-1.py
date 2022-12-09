6d5a43cd056c5be91b8d3835b0916c96be3dee72
Jan <Malion@gmx.net>
2022-12-09 23:48:49 +0100

    Bugfix

--- day_09-1.py
+++ day_09-1.py
@@ -1,7 +1,6 @@
 #region: imports
 import numpy as np
 import time
-import copy
 start = time.time()
 #endregion: imports
 #region: additional functions
@@ -17,7 +16,7 @@ def mov_head(head,instruction):
     return head
 
 def follow(head,tail):
-    if any([abs(head[idx]-tail[idx])==2 for idx in range(2)]) and any([abs(head[idx]-tail[idx])==1 for idx in range(2)]):
+    if any([abs(head[idx]-tail[idx])==2 for idx in range(2)]) and any([abs(head[idx]-tail[idx])==1 for idx in range(2)]) or all([abs(head[idx]-tail[idx])==2 for idx in range(2)]):
         tail = (tail[0]+1*np.sign(head[0]-tail[0]),tail[1]+1*np.sign(head[1]-tail[1]))
     elif abs(head[0]-tail[0])==2:
         tail = (int(tail[0]+((head[0]-tail[0])/2)), tail[1])
@@ -32,10 +31,7 @@ def move_rope(rope_len,input):
         for _ in range(int(line.split(' ')[1])):
             positions[0] = mov_head(positions[0],line.split(' ')[0])
             for idx in range(1,rope_len):
-                tmp = follow(positions[idx-1],positions[idx])
-                if tmp == positions[idx]:
-                    break
-                positions[idx] = tmp
+                positions[idx] = follow(positions[idx-1],positions[idx])
             visited.add(positions[-1])
     return len(visited)
 #endregion: addtional functions
