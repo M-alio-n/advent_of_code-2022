fec25f76ff85b0d9a88447fb0ecb1af28523517d
Jan <Malion@gmx.net>
2022-12-18 00:44:13 +0100

    Couldn't sleep w/o implementing this optimization

--- day_16.py
+++ day_16.py
@@ -1,9 +1,17 @@
 #region: imports
 import time
 import numpy as np
+import functools
 #endregion: imports
 #region: additional functions
 start = time.time()
+def comparisson(a, b):
+    if a[2] > b[2]:
+        return 1
+    elif a[2] < b[2]:
+        return -1
+    else:
+        return 0
 
 def fastest_way(origin,goal):
     ways = [[origin]]
@@ -36,6 +44,31 @@ def determine_value(actionlist):
 def best_combination(time_limit):
     # the tuples descirbe: order of the opening, time it takes, value
     tmp_combinations = [([0],1,0)]
+    max_value = 0
+    combinations = []
+    change_flag = 1
+    while change_flag:
+        combinations = []
+        change_flag = 0
+        for combination in tmp_combinations:
+            combination_changed_flag = 0
+            for idx,next in enumerate(useful_valves):
+                if not idx in combination[0]:
+                    if combination[1]+1+distances[combination[0][-1],idx] < time_limit:
+                        # If this valve was not yet visited and the resulting time is below the time limit add it to the list
+                        combinations.append((combination[0]+[idx], combination[1]+1+distances[combination[0][-1],idx], combination[2]+useful_capacities[idx]*(time_limit-(combination[1]+distances[combination[0][-1],idx]))))
+                        change_flag = 1
+                        combination_changed_flag = 1
+                        if combinations[-1][2] > max_value:
+                            max_value = combinations[-1][2]
+        if change_flag:
+            tmp_combinations = combinations
+    return max_value
+
+def all_combinations(time_limit):
+    # the tuples descirbe: order of the opening, time it takes, value
+    tmp_combinations = [([0],1,0)]
+    final_combinations = tmp_combinations
     combinations = []
     change_flag = 1
     while change_flag:
@@ -55,15 +88,16 @@ def best_combination(time_limit):
                 combinations.append(combination)
         if change_flag:
             tmp_combinations = combinations
+            final_combinations = final_combinations+combinations
     max_value = 0
     for combination in tmp_combinations:
         if combination[2] > max_value:
             max_value = combination[2]
-    return (max_value,tmp_combinations)
+    return (max_value,final_combinations)
 
 #endregion: addtional functions
 #region: load input
-input = open('16t').read().split('\n')
+input = open('16').read().split('\n')
 valves = {}
 useful_valves = ['AA']
 useful_capacities = [0]
@@ -81,26 +115,37 @@ for count,valve in enumerate(useful_valves):
             continue
         distances[count,count_2] = len(fastest_way(valve,valve_2))-1
 
-best_value, combinations = best_combination(30)
+best_value = best_combination(30)
 print(f'Solution for part 1: {best_value}')
 part1 = time.time()
 #endregion: part 1
 #region: part 2
-
 '''This solution does not work for the testinput, because best_combination only returns
 combinations that utilize the complete time that is available. For the optimal solution
 of the testinput both, the elephant and I, would just work 11 minutes of the 26.
 I might include these cases, though it will increase the already long runtime...
 '''
+_, combinations = all_combinations(26)
+
+combinations = sorted(combinations, key=functools.cmp_to_key(comparisson),reverse=True)
+best_val = 0
+first_hit_flag = 0
+for count,comb_1 in enumerate(combinations):
+    if first_hit_flag:
+        if comb_1[2] < best_val/2:
+            # once we have one correct hit, we don't need to go further down than to the half of this value
+            break
+    for comb_2 in combinations[count+1::]:
+        if not any([valve in comb_2[0] for valve in comb_1[0][1::]]):
+            # legal hit!
+            if comb_2[2]+comb_1[2] > best_val:
+                best_val = comb_2[2]+comb_1[2]
+                first_hit_flag = 1
+            if first_hit_flag:
+                if comb_2[2] < best_val/2:
+                    # once we have one correct hit, we don't need to go further down than to the half of this value
+                    break
 part2 = time.time()
-_, combinations = best_combination(26)
-best_value = 0
-for count1, combination_1 in enumerate(combinations):
-    print(f'Checked {count1} of {len(combinations)} current solution: {best_value}')
-    for count2,combination_2 in enumerate(combinations):
-        if not any([valve in combination_2[0] for valve in combination_1[0][1::]]):
-            if combination_1[2]+combination_2[2]> best_value:
-                best_value = combination_1[2]+combination_2[2]
-print(f'Solution for part 2: {best_value}')
+print(f'Solution for part 2: {best_val}')
 print(f'Total time: {part2-start}, part 1 time: {part1-start}, part 2 time: {part2-part1}')
 #endregion: part 2
